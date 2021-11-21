befcede6bff53246483b73048dd61c9272d71740
Tim Middleton <x@vaults.ca>
2021-11-20 22:18:19 -0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,223 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Button, Grid, Slider, Stack, Typography, Box, IconButton } from '@mui/material';
-import { ArrowBack, ArrowLeft, ArrowRight } from '@mui/icons-material';
+import { Button, Grid, Slider, Stack, Typography, Box, IconButton, Checkbox, CssBaseline, TextField } from '@mui/material';
+import { ArrowLeft, ArrowRight } from '@mui/icons-material';
+import { ThemeProvider } from '@mui/system';
+import { theme1 } from './themes';
 
-const styles = {
+const DEFAULT_QUANTITY: number = 1
+const DEFAULT_MIN: number = 1;
+const DEFAULT_MAX: number = 10;
+const MAX_QUANTITY: number = 100;
+const MAX_HISTORY: number = MAX_QUANTITY * 5;
 
+type randomType = {
+  value: number,
+  min: number, 
+  max: number,
+  time: Date,
+  exclusive: boolean,
 }
 
-const MAX_HISTORY = 100;
-interface stateType {
+type stateType = {
   min: number,
   max: number,
   sliderValue: number,
   exclusive: boolean,
-  randomNumber: number|null,
+  zeroBase: boolean,
+  randoms: randomType[],
   lastTime: Date,
-  previousNumbers: number[][],
   previousRange: number[][],
+  quantity: number,
+  newQuantity: number,
 }
 
 const initState: stateType = {
-  min: 1,
-  max: 100,
-  sliderValue: 10,
+  min: DEFAULT_MIN,
+  max: DEFAULT_MAX,
+  sliderValue: DEFAULT_MAX,
   exclusive: false,
-  randomNumber: null,
+  zeroBase: false,
+  randoms: [],
   lastTime: new Date(),
-  previousNumbers: [],
   previousRange: [],
+  quantity: DEFAULT_QUANTITY,
+  newQuantity: DEFAULT_QUANTITY,
 }
 
+// generate new randoms numbers to the front of the history list
 function generate(state: stateType, min?: number, max?: number): stateType {
 
   if (min === undefined) min = state.min;
   if (max === undefined) max = state.max;
 
   const newState = {...state, min: min, max: max};
+  const exclusive_max = state.exclusive ? max - 1 : max
 
-  if (state.exclusive) max--;
+  if (state.newQuantity !== state.quantity) newState.quantity = state.newQuantity;
 
-  newState.randomNumber = Math.floor(Math.random() * (max + 1 - min) + min);
-  newState.previousNumbers.unshift([newState.randomNumber, min, max]);
-  while (newState.previousNumbers.length>MAX_HISTORY) newState.previousNumbers.pop();
-  newState.lastTime = new Date();
-  newState.sliderValue = newState.max;
+  for (let i=0; i < newState.quantity; i++) {
+    newState.randoms.unshift({
+      value: Math.floor(Math.random() * (exclusive_max + 1 - min) + min),
+      min: min, 
+      max: max,
+      time: new Date(),
+      exclusive: state.exclusive,
+    })
+    if (newState.randoms.length > MAX_HISTORY) newState.randoms.pop();
+  }
+
+  newState.lastTime = newState.randoms[0].time;
+  newState.sliderValue = max;
   return newState;
 }
+
 
 function App() {
 
   const [state, setState] = useState(initState)
 
-  function handleSliderChange(event: Event, value: number|number[], thumb: number) {
+  // make sure the limit values are sane, then update state
+  function handleLimitChange(value: number, type: "lower"|"upper_reset"|"upper"): stateType {
+    const newState: stateType = {...state};
+
+    if (type === 'lower') {
+      const closest_min: number = state.exclusive ? newState.max - 2 : newState.max - 1;
+      if (isNaN(value)) value = state.zeroBase ? 0 : DEFAULT_MIN;
+      if (value > closest_min) value = closest_min;
+      newState.min = value
+    }
+    else {
+      const closest_max: number = state.exclusive ? newState.min + 2 : newState.min + 1;
+      if (isNaN(value)) value = DEFAULT_MAX;
+      if (value < closest_max) {
+        if (type === "upper_reset") {
+          const default_min = state.zeroBase ? 0 : DEFAULT_MIN;
+          const min_max = state.exclusive ? default_min + 2 : default_min + 1;
+          if (value >= min_max) {
+            newState.min = default_min;
+          }
+          else {
+            value = closest_max;
+          }
+        }
+        else {
+          value = closest_max;
+        }      
+      }
+      newState.max = value
+    }
+    return newState;
+  }
+
+  function handleSliderChange(value: number|number[], thumb: number): stateType {
     console.log("thumb ", thumb, ": ", value);
-    setState({...state, sliderValue:value as number, max: value as number});
+    const newState: stateType = {...state}
+    if (thumb < 0) newState.min = value as number;
+    else {
+      newState.sliderValue = value as number;
+      newState.max = value as number;
+    }
+    return newState;
   }
 
   function handleMaxButton(event: React.MouseEvent<HTMLElement>, value: number) {
-    setState(generate(state, state.min, value));
+    const newState = handleLimitChange(value, "upper_reset");
+    setState(generate(newState));
+  }
+
+  type QuickButtonProps = {
+    children: React.ReactNode,
+    values: number[]
+  }
+  
+  function QuickButtons({values, children}: QuickButtonProps): JSX.Element {
+    return (
+      <>
+      <Typography>{children}</Typography>
+      <Box sx={{display: "flex", flexDirection: "row", flexWrap: "wrap"}}>
+        {values.map((v) => (
+        <Button key={v} variant="contained" sx={{margin:"1px"}} onClick={(e) => handleMaxButton(e, v)}
+            color={state.max === v ? "secondary" : "primary"}>{v}</Button>))}
+      </Box>
+    </>
+    );
+  }
+
+  function DisplayButton(props: any) {
+    return (
+      <Button sx={{height:"10em", padding:"1em 5em 1em 5em"}} 
+          variant="outlined" 
+          onClick={() => setState(generate(state))}>
+        <Typography component="h2" variant="h2">{props.children}</Typography>
+      </Button>
+    )
   }
 
   console.log("slider render", state)
   return (
+    <ThemeProvider theme={theme1}>
     <div className="App">
+      <CssBaseline/>
       <header className="App-header">
+      <Typography component="h1" variant="h5" sx={{textAlign:"center", marginBottom:"0.5em"}}>Rapidly Random Numbers</Typography>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-          <Button sx={{height:"10em", padding:"1em"}} onClick={() => setState(generate(state))}><Typography component="h2" variant="h2">{state.randomNumber === null ? "?" : state.randomNumber}</Typography></Button>
-          <Typography><i>{state.min} to {state.max} (inclusive)</i></Typography>
-          { (state.randomNumber === null) ? "" : (<><Typography sx={{fontSize:"50%"}}>{state.lastTime.toString()}</Typography></>) }
+            { state.randoms.slice(0, state.quantity).map((e:randomType) => (
+              <DisplayButton>{e.value}</DisplayButton>
+            ))}
+            { (state.randoms.length !== 0) ? "" : (
+              <DisplayButton>Press Here!</DisplayButton>) }
+          <Typography sx={{marginTop: "0.5em"}}><i>{state.min} to {state.max} (inclusive)</i></Typography>
+          { (state.randoms.length === 0) ? "" : (<><Typography sx={{fontSize:"50%"}}>{state.lastTime.toString()}</Typography></>) }
           </Grid>
-          { (state.previousNumbers.length === 0) ? "" : (
+          { (state.randoms.length <= state.quantity) ? "" : (
             <Grid item xs={12}>
-              <Typography>Previous: {state.previousNumbers.slice(1,11).map((i) => i[0]).join(", ")}</Typography>
+              <Typography>Previous: {state.randoms.slice(state.quantity,state.quantity+11).map((i) => i.value).join(", ")}</Typography>
             </Grid>
           )}
           <Grid item xs={12}>
             <Stack direction="row" justifyContent="center" spacing={1}>
-              
-              <Typography>Highest Number:</Typography>
-              <Box sx={{display: "flex", flexDirection: "row"}}>
-              {[2, 6, 10, 12, 20, 50, 100, 1000, 10000, 100000, 1000000].map((v) => (
-                  <Button variant="contained" onClick={(e) => handleMaxButton(e, v)}
-                      color={state.max === v ? "secondary" : "primary"}>{v}</Button>))}
-              </Box>
+              <QuickButtons values={[2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 20, 30, 50, 100, 256, 1000, 10000, 100000, 1000000]}>
+                Highest Number:
+              </QuickButtons>
             </Stack>
           </Grid>
           <Grid item xs={12}>
               <Stack direction="row" justifyContent="center">
-                <IconButton sx={{marginTop:-1}} aria-label="decrease highest number" disabled={state.sliderValue <= state.min}><ArrowLeft/></IconButton>
+                <IconButton sx={{marginTop:-0.5}} aria-label="decrease highest number" disabled={state.sliderValue <= state.min} onClick={(e) => setState(handleSliderChange(state.sliderValue-1, 0))}><ArrowLeft/></IconButton>
             <Slider marks={false} disableSwap
                 sx={{width:"50%"}}
-                min={1} 
+                min={2} 
                 max={100}
                 value={state.sliderValue}
-                valueLabelDisplay="on"
-                onChange={handleSliderChange}/>
-                <IconButton sx={{marginTop:-1}} aria-label="increase highest number" disabled={state.sliderValue >= 100} onClick={() => setState({...state, sliderValue:state.sliderValue+1})}><ArrowRight/></IconButton>
+                valueLabelDisplay="auto"
+                onChange={(e,v,t) => setState(handleSliderChange(v, t))}/>
+                <IconButton sx={{marginTop:-0.5}} aria-label="increase highest number" disabled={state.sliderValue >= 100} onClick={(e) => setState(handleSliderChange(state.sliderValue+1, 0))}><ArrowRight/></IconButton>
             </Stack>
+          </Grid>
+          <Grid item xs={12}>
+            <Checkbox></Checkbox>
+
+          </Grid>
+          <Grid item xs={12}>
+            <TextField type="number" id="set-min" size="small" value={state.min} 
+                label="Lowest" sx={{width:"6em"}}
+                onChange={(e) => setState(handleLimitChange(parseInt(e.target.value), "lower"))}
+                InputLabelProps={{shrink: true}} />
+            <TextField type="number" id="set-max" size="small" value={state.max} 
+                label="Highest" sx={{width:"6em"}}
+                onChange={(e) => setState(handleLimitChange(parseInt(e.target.value), "upper"))}
+                InputLabelProps={{shrink: true}} />
+            <TextField type="number" id="set-quantity" size="small" value={state.newQuantity} 
+                label="Quantity" sx={{width:"6em"}}
+                onChange={(e) => {setState({...state, newQuantity:parseInt(e.target.value)||DEFAULT_QUANTITY})}}
+                InputLabelProps={{shrink: true}} />
           </Grid>
         </Grid>
       </header>
     </div>
+    </ThemeProvider>
   );
 }
 
